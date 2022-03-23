bae1f2000952862aeb97a484e92662b042855837
Owen Dresner <owendrez@gmail.com>
2022-03-22 20:09:57 -0400

    commit tree 4b0a861736e2f33161f63aa3596ec4b1f653a309

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+const cards=["placeholder","placeholder","placeholder"]
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    
+    cards[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+
+  return cards;
+}
+function countDown(number){
+    
+    for (let countup = number; countup >-1; countup--) {
+        console.log(countup);
+      }
+}
